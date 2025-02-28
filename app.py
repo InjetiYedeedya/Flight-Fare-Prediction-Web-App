134a08d903077c067187f8b73041992642fc664b
InjetiYedeedya <126330919+InjetiYedeedya@users.noreply.github.com>
2025-02-28 19:19:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,6 @@ def predict():
         return redirect(url_for('home'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from os import environ
+    port = int(environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
